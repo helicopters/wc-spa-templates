5e670bc585b61f93e4da735d959ea91a2036874e
林丰 <linhui@linfengdeMacBook-Air.local>
2017-12-19 01:08:32 +0800

    修改模板

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 
 import App from './App'
 
+/* 引入 vconsole */
+if (process.env.NODE_ENV === 'development') {
+    var VConsole = require('vconsole/dist/vconsole.min.js');
+}
+
 new Vue({
     el: '#app',
     router,
---
+++ tpl.vue
@@ -0,0 +1,29 @@
+<style lang="less">
+	
+</style>
+<template>
+	<div class="container">
+		
+	</div>
+</template>
+<script>
+	export default {
+		components: {
+			
+		},
+		data () {
+			return {
+
+			}
+		},
+		computed: {
+
+		},
+		mounted () {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
