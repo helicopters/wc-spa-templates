4d484e3feb3d58966fb73d8a12ce4809a4e8426a
林丰 <linhui@linfengdeMacBook-Air.local>
2017-12-21 23:45:19 +0800

    添加 tools

--- src/modules/tools/index.js
+++ src/modules/tools/index.js
@@ -1,1 +1,11 @@
-index.js
+import isNotProduction from './is-not-production'
+import isDevelopment from './is-development'
+import setTitle from './set-title'
+import href from './href'
+
+window.tools = {
+	isNotProduction,
+	isDevelopment,
+	setTitle,
+	href
+}
