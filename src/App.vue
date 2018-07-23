274c3eb7bb4befa48e054dcae2382e5f319d9cd0
林丰 <>
2018-07-23 16:12:46 +0800

    开始进行webpack4升级

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
 	<div id="app">
-		<!-- 主要为了微信分享, 为所有页面注入默认的分享信息 -->
-		<!-- <wx-share/> -->
 		<router-view></router-view>
 	</div>
 </template>
 <script>
-export default {
-}
+  export default {
+  }
 </script>
